f43bfb96ac03acc75a87218a4e6925cd3f676ca7
Jon Love <jon_w-l@hotmail.com>
2025-11-19 09:37:26 +0800

    Refactor code

--- main.go
+++ main.go
@@ -3,9 +3,43 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		curLine := ""
+
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				curLine += string(data[:i])
+				ch <- curLine
+				data = data[i+1:]
+				curLine = ""
+			}
+			curLine += string(data)
+		}
+		if len(curLine)!= 0 {
+			ch <- curLine
+		}
+	}()
+
+	return ch
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
@@ -14,25 +48,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	curLine := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
+	ch := getLinesChannel(f)
 
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			curLine += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", curLine)
-			curLine = ""
-		}
-		curLine += string(data)
+	for l := range ch {
+		fmt.Printf("read: %s\n", l)
 	}
 
-	if len(curLine) != 0 {
-		fmt.Printf("read: %s\n", curLine)
-	}
 }
