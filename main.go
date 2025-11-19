7758d63793428410bfcd2b2c27ffd4d9fd829afa
Jon Love <jon_w-l@hotmail.com>
2025-11-19 09:24:26 +0800

    Finish reading line by line

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -13,6 +14,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	curLine := ""
 	for {
 		data := make([]byte, 8)
 		n, err := f.Read(data)
@@ -20,7 +22,17 @@ func main() {
 		if err != nil {
 			break
 		}
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			curLine += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", curLine)
+			curLine = ""
+		}
+		curLine += string(data)
+	}
 
-		fmt.Printf("read: %s\n", string(data[:n]))
+	if len(curLine) != 0 {
+		fmt.Printf("read: %s\n", curLine)
 	}
 }
