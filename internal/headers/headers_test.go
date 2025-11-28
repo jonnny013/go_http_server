abc6a14b61c55f7ebac0e5b95f4c02dd36069cb2
Jon Love <jon_w-l@hotmail.com>
2025-11-28 11:19:25 +0800

    Add multi header test

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -25,4 +25,14 @@ func TestHeaderParsing(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+		// Test: Valid multi header
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nSomething: cool\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
 }
